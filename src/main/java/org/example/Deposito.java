4fbd3a98e20dfc882458b7a077a954b79587e06a
jgodoy2022@ <jgodoy2022@udec.cl>
2023-10-11 13:43:24 -0300

    Cambios en deposito a Array generico T, y Creacion de la clase Producto y Dulce

--- src/main/java/org/example/Deposito.java
+++ src/main/java/org/example/Deposito.java
@@ -1,28 +1,17 @@
 package org.example;
 import java.util.ArrayList;
 
-public class Deposito {
-    private ArrayList<Bebida> m;
-    private ArrayList<Moneda> l;
+public class Deposito<T> {
+    private ArrayList<T> Cosas;
     public Deposito(){
-        m = new ArrayList();
-        l = new ArrayList();
+        Cosas = new ArrayList();
     }
-    public void addBebida(Bebida b){
-        m.add(b);
+    public void addCosas(T b){
+        Cosas.add(b);
     }
-    public Bebida getBebida(){
-        if(m.size() != 0){
-            return m.remove(0);
-        }
-        return null;
-    }
-    public void addMoneda(Moneda m){
-        l.add(m);
-    }
-    public Moneda getMoneda(){
-        if(l.size() != 0){
-            return l.remove(0);
+    public T getCosas(){
+        if(Cosas.size() != 0){
+            return Cosas.remove(0);
         }
         return null;
     }
--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -1,5 +1,5 @@
 package org.example;
 
-public class Producto {
+public abstract class Producto {
 
 }
