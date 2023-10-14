29f576f839ef22337a947a4a573fe955f3d7d8cc
jgodoy2022@ <jgodoy2022@udec.cl>
2023-10-14 16:51:18 -0300

    Arreglos en comprador por falla en el vuelto

--- src/main/java/org/example/Comprador.java
+++ src/main/java/org/example/Comprador.java
@@ -1,23 +1,24 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class Comprador {
     private String sonido;
     private int vuelto;
-
 
     public Comprador(Moneda m, int cualProducto, Expendedor exp) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         Producto b = exp.comprarProducto(m, cualProducto);
         if(cualProducto == TipoProducto.COCA.getOpcion() || cualProducto == TipoProducto.SPRITE.getOpcion()
                 || cualProducto == TipoProducto.FANTA.getOpcion() || cualProducto == TipoProducto.SNICKERS.getOpcion()
                 || cualProducto == TipoProducto.SUPER8.getOpcion()){
+            int aux = 0;
+            while(exp.getVuelto() != null){
+                aux += 100;
+            }
             if(b != null){
                 sonido = b.sabor();
-                vuelto = ?
+                vuelto = aux;
             }
             else if(b == null && m != null){
-                vuelto = ?
+                vuelto = aux;
                 throw new NoHayProductoException("Error. Producto no disponible");
             }
         }
@@ -29,7 +30,7 @@ public class Comprador {
     public int cuantoVuelto(){
         return vuelto;
     }
-    public String queBebiste(){
+    public String queCompraste(){
         return sonido;
     }
 }
