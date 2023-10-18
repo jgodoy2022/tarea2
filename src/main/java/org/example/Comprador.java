e2be8d25e30121852290f616373ed70d93649d67
jgodoy2022@ <jgodoy2022@udec.cl>
2023-10-18 13:06:19 -0300

    docs: Bebida y Comprador documentacion

--- src/main/java/org/example/Comprador.java
+++ src/main/java/org/example/Comprador.java
@@ -1,9 +1,26 @@
 package org.example;
 
+/**
+ * La clase Comprador representa a un cliente que realiza una compra a traves de un expendedor
+ * @author Joaquin Alejandro Godoy Vergara
+ * @author Gabriela Isidora Zambrano Novoa
+ * @see NoHayProductoException,PagoInsuficienteException,PagoIncorrectoException
+ */
 public class Comprador {
+    /** String para almacenar el "sonido" del producto comprado*/
     private String sonido;
+
+    /** Entero para almacenar el vuelto de la compra*/
     private int vuelto;
 
+    /** Constructor, recibe tres parametros para realizar la compra del producto y almacenar el sonido y calcular el vuelto
+     * @param m representa la moneda usada para el pago
+     * @param cualProducto indica el producto a comprar
+     * @param exp hace referencia al expendedor para realizar la compra
+     * @throws NoHayProductoException si no hay producto disponible o si es invalido
+     * @throws PagoIncorrectoException si el pago es invalido
+     * @throws PagoInsuficienteException si el pago es insuficiente
+     */
     public Comprador(Moneda m, int cualProducto, Expendedor exp) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         Producto b = exp.comprarProducto(m, cualProducto);
         if(cualProducto == TipoProducto.COCA.getOpcion() || cualProducto == TipoProducto.SPRITE.getOpcion()
@@ -27,9 +44,13 @@ public class Comprador {
             throw new NoHayProductoException("Error. Producto no disponible");
         }
     }
+
+    /** @return vuelto calculado en el constructor*/
     public int cuantoVuelto(){
         return vuelto;
     }
+
+    /** @return sonido almacenado en el constructor*/
     public String queCompraste(){
         return sonido;
     }
